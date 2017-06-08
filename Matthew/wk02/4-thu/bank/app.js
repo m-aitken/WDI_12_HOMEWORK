fff745b7b0469b431861f2ff65b925677dd980e5
Matt Aitken <matt.aitken@gmail.com>
2017-06-08 22:13:34 +1000

    page, style, initial js

---
+++ Matthew/wk02/4-thu/bank/app.js
@@ -0,0 +1,34 @@
+// linking up balance log, input fields, buttons 
+
+var savingsBalance = document.querySelector('#saveBalDiv');
+var savingsAmount = document.querySelector('#saveAmt');
+var savingsWithdrawBtn = document.querySelector('#saveWithdrawButton');
+var savingsDepositBtn = document.querySelector('#saveDepositButton');
+
+var checkBalance = document.querySelector('#checkBalDiv');
+var checkAmount = document.querySelector('#checkAmt');
+var checkWithdrawBtn = document.querySelector('#checkWithdrawButton');
+var checkDepositBtn = document.querySelector('#checkDepositButton');
+
+
+savingsWithdraw
+
+
+savingsDeposit
+
+
+
+checkWithdraw
+
+
+
+checkDeposit
+
+
+
+// listening to button clicks
+
+savingsWithdrawBtn.addEventListener();
+savingsDepositBtn.addEventListener();
+checkWithdrawBtn.addEventListener();
+checkDepositBtn.addEventListener();
