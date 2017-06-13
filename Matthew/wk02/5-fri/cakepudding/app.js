84e35d1a0c4a3783ad185205a3346d66c80825ec
Matt Aitken <matt.aitken@gmail.com>
2017-06-13 10:46:38 +1000

    add commentary

--- Matthew/wk02/5-fri/cakepudding/app.js
+++ Matthew/wk02/5-fri/cakepudding/app.js
@@ -2,19 +2,23 @@
 function getRandomInt(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 };
-
+// source list
 var list = ["virgin", "qantas", "jetstar", "tigerair", "etihad"];
-
+// list length
 var index = list.length;
 
+//get random index key in list array
 var randomIndex = getRandomInt(0, index-1);
 
+//get random word using random index key
 var word = list[randomIndex].toUpperCase().split("");
 
+// random word length
 var count = word.length;
 
 var guessCount = 9;
 
+// for word length, create listItems as " _ "
 for (var i=0; i < count; i++){
   var newListItem = document.createElement('li');
   newListItem.textContent = "_";
@@ -22,36 +26,39 @@ for (var i=0; i < count; i++){
 };
 
 
+// listen on page for KB
 document.body.addEventListener('keydown', function(event){
 
 
   var isInWord = false;
+
+  // letter equals the KB key pressed as string
   var letter = String.fromCharCode(event.keyCode);
   var correctGuess = document.querySelector('.hangman').getElementsByTagName('li');
 
   document.querySelector('h5').textContent = "Turns Left: " + guessCount.toString();
 
   for(j=0; j< count; j++){
-    if (word[j] === letter){
+    if (word[j] === letter){    // loop through word letters, check if same as key press
       isInWord = true;
-      correctGuess[j].textContent = letter;
-      word[j] ='';
+      correctGuess[j].textContent = letter;   // replace for correct letter
+      word[j] ='';      // for that correct letter, remove from word
     }
   }
 
   if(!isInWord){
-    var newListItem = document.createElement('li');
-    newListItem.textContent = letter;
-    document.querySelector('.wrong-guesses').appendChild(newListItem);
+    var newListItem = document.createElement('li');  // create new list item
+    newListItem.textContent = letter;     // list item is letter pressed
+    document.querySelector('.wrong-guesses').appendChild(newListItem);  // add letter to wrong guesses list
   }
 
-  if(word.join('') === ''){
+  if(word.join('') === ''){     // if word is successfully cleared, game won
     document.querySelector('h3').textContent = "You got it!";
   }
-  else if(guessCount === 0){
+  else if(guessCount === 0){    // if guess count runs out game is lost
     document.querySelector('h3').textContent = "Try again!";
   }
-
+  // each loop remove 1 from guess counter
   guessCount--;
 
 });
