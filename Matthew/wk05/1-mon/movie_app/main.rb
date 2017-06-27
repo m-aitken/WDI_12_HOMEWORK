1545bf0d8961a06984b4a38e39902875b71ed79b
Matt Aitken <matt.aitken@gmail.com>
2017-06-27 23:10:40 +1000

    movies 2 workings

--- Matthew/wk05/1-mon/movie_app/main.rb
+++ Matthew/wk05/1-mon/movie_app/main.rb
@@ -11,16 +11,16 @@ get '/' do
 end
 
 
-get '/result' do
+get '/search_result' do
   api = "http://omdbapi.com/?s=#{params[:search]}&apikey=2f6435d9"
   parsed = HTTParty.get(api).parsed_response
   @results = parsed['Search']
-      
-  # results array - loop thru, print results
-  # @results[array]['Title'] returns Title
-  binding.pry
-  erb :result
+  erb :result 
 end
 
-
-
+# #{params[:search]}
+get '/result_details' do
+  api = "http://omdbapi.com/?t=#{params[:title]}&apikey=2f6435d9"
+  @details = HTTParty.get(api).parsed_response
+  erb :details
+end
