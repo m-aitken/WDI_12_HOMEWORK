336b4b0da68373425762348fa0e8131a7e8903c6
Matt Aitken <matt.aitken@gmail.com>
2017-07-09 20:05:19 +1000

    list options

--- Matthew/wk06/5-fri/citipix/js/main.js
+++ Matthew/wk06/5-fri/citipix/js/main.js
@@ -1,1 +1,15 @@
-var cities = []
+var cities = ["Austin", "Los Angeles", "New York", "San Francisco", "Sydney"]
+var option = document.getElementById("city-type")
+
+for (var i=0; i<cities.length; i++) {
+  var select = cities[i];
+  var element = document.createElement("option");
+  element.textContent = select;
+  element.value = select;
+  option.appendChild(element);
+}
+
+
+
+// if citytype value = cityname
+  
